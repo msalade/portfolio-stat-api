b40592892a0985fa7f48564d211d632925606abe
salat97 <salaciak.michal@gmail.com>
2020-05-08 20:11:18 +0200

    added authentication

---
+++ src/auth.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+import admin from 'firebase-admin';
+
+const databaseURL = process.env.FIREBASE_DATABASE;
+const serviceAccount = require('../fbServiceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL
+});
+
+const auth = (req: Request, res: Response, next: NextFunction) => {
+    if (req.headers.authtoken) {
+        admin
+            .auth()
+            .verifyIdToken(req.headers.authtoken as string)
+            .then(() => {
+                next();
+            })
+            .catch(() => {
+                res.status(403).send('Unauthorized');
+            });
+    } else {
+        res.status(403).send('Unauthorized');
+    }
+};
+
+export default auth;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
-import express from "express";
+require('dotenv').config();
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from 'cors';
+
+import auth from './auth';
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello worldddsss!");
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/*', auth);
+
+app.get('/', (req, res) => {
+    res.send('Hello worldddsss!');
 });
 
 app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
+    console.log(`server started at http://localhost:${port}`);
 });
